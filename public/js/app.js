44c2f2b4b8e12760e83ffce058127847ae302d17
benjamin lam <benjamin-lam@reclaim-your-data.org>
2026-02-13 13:50:41 +0100

    chore: 3, 2, 1... let's jam! 🎷 (initial project scaffolding)

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Bootstrapping kommt später
+    console.log('Reclaim Your Data Client – (Sub-Grid-Echo) - v0.1');
+});
---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// v0 – Platzhalter, echte Caching-Logik kommt später
+self.addEventListener('install', () => {
+    // noop
+});
